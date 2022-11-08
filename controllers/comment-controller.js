366a6ea2d45f306fa48435a0cc289de9eac2e0c4
Daniela Lopez <dlope0831@gmail.com>
2022-11-07 18:03:00 -0600

    added data validation feature

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -1,3 +1,4 @@
+const { trusted } = require('mongoose');
 const { Comment, Pizza } = require('../models');
 
 const commentController = {
@@ -26,7 +27,7 @@ const commentController = {
         Comment.findOneAndUpdate(
           { _id: params.commentId },
           { $push: { replies: body } },
-          { new: true }
+          { new: true, runValidators: true }
         )
           .then(dbPizzaData => {
             if (!dbPizzaData) {
